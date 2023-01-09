17ff16f54958cec18af358a16d067260a731bc7b
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2023-01-09 16:21:07 +0530

    Producer

---
+++ producer/src/main/java/com/example/mq/controller/MessagePublisher.java
@@ -0,0 +1,30 @@
+package com.example.mq.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mq.config.MQConfig;
+import com.example.mq.model.CustomMessage;
+
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+public class MessagePublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @PostMapping("/publish")
+    public String publishMessage(@RequestBody CustomMessage message) {
+        message.setMessageId(UUID.randomUUID().toString());
+        message.setMessageDate(new Date());
+        template.convertAndSend(MQConfig.EXCHANGE,
+                MQConfig.ROUTING_KEY, message);
+
+        return "Message Published";
+    }
+}
